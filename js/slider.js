15278283f4962ee5b577adfa6eb1c5fa29d713ed
Angela Ponomarenko <angelkapv81@gmail.com>
2022-12-24 00:32:51 +0200

    fix slaider

---
+++ js/slider.js
@@ -0,0 +1,19 @@
+$(".gallery-slider").slick({
+  dots: !1,
+  infinite: !0,
+  speed: 1e3,
+  slidesToShow: 1,
+  adaptiveHeight: !0,
+  autoplay: !0,
+  autoplaySpeed: 2e3,
+}),
+  $(".customers-slider").slick({
+    dots: !1,
+    infinite: !0,
+    speed: 1e3,
+    slidesToShow: 1,
+    adaptiveHeight: !0,
+    autoplay: !0,
+    autoplaySpeed: 2e3,
+  });
+//# sourceMappingURL=index.d3b38a25.js.map
